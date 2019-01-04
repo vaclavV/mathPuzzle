6420e67d9c74980fc78bed322192dbd60596d923
vaclav <votrv1ap@pedf.cuni.cz>
2019-01-05 00:20:28 +0100

    prepinani barev segmentu

--- hriste.js
+++ hriste.js
@@ -1,5 +1,14 @@
-/* 
 
- */
 
+    
+var color=[1,1,1,1,1,1,1,1];
+
+function changeColor(elmnt, no){
+    if(color[no]==1){
+ elmnt.style.fill='#000';
+color[no]=0;
+    }else{ elmnt.style.fill='#EFE';
+ color[no]=1;}
+    }
+ 
 
