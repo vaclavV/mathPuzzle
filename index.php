93053b34b75e7212fcff268e2e4672337b25aa64
vaclav <votrv1ap@pedf.cuni.cz>
2019-01-05 21:44:26 +0100

    index umi nakreslit obrazky z DB

--- index.php
+++ index.php
@@ -38,45 +38,98 @@
                 </div></div>
             <div class="row">
                 <div class="col-sm-4">
-                    <svg width="120" height="120" class="segment" id=""></svg>
+                    <svg width="120" height="120" class="segment" id=""> 
+                    <?php
+                    include './Segment.php';
+                    $segment1 = new Segment(1, 'test');
+                    $segment1->Vypis();
+                    ?></svg>
                     <span class="priklad">lorem ipsum</span><br>
-                    <svg width="120" height="120" class="segment" id=""></svg>
+                    <svg width="120" height="120" class="segment" id="">
+                    <?php
+                    $segment2 = new Segment(2, 'test');
+                    $segment2->Vypis();
+                    ?>
+                    </svg>
                     <span class="priklad">lorem ipsum</span><br>
-                    <svg width="120" height="120" class="segment" id=""></svg>
+                    <svg width="120" height="120" class="segment" id=""><?php
+                    $segment3 = new Segment(3, 'test');
+                    $segment3->Vypis();
+                    ?></svg>
                     <span class="priklad">lorem ipsum</span><br>
-                    <svg width="120" height="120" class="segment" id=""></svg>
+                    <svg width="120" height="120" class="segment" id=""><?php
+                    $segment4 = new Segment(4, 'test');
+                    $segment4->Vypis();
+                    ?></svg>
                     <span class="priklad">lorem ipsum</span><br>
-                    <svg width="120" height="120" class="segment" id=""></svg>
+                    <svg width="120" height="120" class="segment" id=""><?php
+                    $segment5 = new Segment(5, 'test');
+                    $segment5->Vypis();
+                    ?></svg>
                     <span class="priklad">lorem ipsum</span><br>
                 </div>
                 <div class="col-sm-4">
-                    <svg width="120" height="120" class="segment" id=""></svg>
+                   <svg width="120" height="120" class="segment" id=""> 
+                    <?php
+                    $segment6 = new Segment(6, 'test');
+                    $segment6->Vypis();
+                    ?></svg>
                     <span class="priklad">lorem ipsum</span><br>
-                    <svg width="120" height="120" class="segment" id=""></svg>
+                    <svg width="120" height="120" class="segment" id="">
+                    <?php
+                    $segment7 = new Segment(7, 'test');
+                    $segment7->Vypis();
+                    ?>
+                    </svg>
                     <span class="priklad">lorem ipsum</span><br>
-                    <svg width="120" height="120" class="segment" id=""></svg>
+                    <svg width="120" height="120" class="segment" id=""><?php
+                    $segment8 = new Segment(8, 'test');
+                    $segment8->Vypis();
+                    ?></svg>
                     <span class="priklad">lorem ipsum</span><br>
-                    <svg width="120" height="120" class="segment" id=""></svg>
+                    <svg width="120" height="120" class="segment" id=""><?php
+                    $segment9 = new Segment(9, 'test');
+                    $segment9->Vypis();
+                    ?></svg>
                     <span class="priklad">lorem ipsum</span><br>
-                    <svg width="120" height="120" class="segment" id=""></svg>
+                    <svg width="120" height="120" class="segment" id=""><?php
+                    $segment10 = new Segment(10, 'test');
+                    $segment10->Vypis();
+                    ?></svg>
                     <span class="priklad">lorem ipsum</span><br>
                 </div>
                 <div class="col-sm-4">        
-                    <svg width="120" height="120" class="segment" id=""></svg>
+                    <svg width="120" height="120" class="segment" id=""> 
+                    <?php
+                    $segment11 = new Segment(11, 'test');
+                    $segment11->Vypis();
+                    ?></svg>
                     <span class="priklad">lorem ipsum</span><br>
-                    <svg width="120" height="120" class="segment" id=""></svg>
+                    <svg width="120" height="120" class="segment" id="">
+                    <?php
+                    $segment12 = new Segment(12, 'test');
+                    $segment12->Vypis();
+                    ?>
+                    </svg>
                     <span class="priklad">lorem ipsum</span><br>
-                    <svg width="120" height="120" class="segment" id=""></svg>
+                    <svg width="120" height="120" class="segment" id=""><?php
+                    $segment13 = new Segment(13, 'test');
+                    $segment13->Vypis();
+                    ?></svg>
                     <span class="priklad">lorem ipsum</span><br>
-                    <svg width="120" height="120" class="segment" id=""></svg>
+                    <svg width="120" height="120" class="segment" id=""><?php
+                    $segment14 = new Segment(14, 'test');
+                    $segment14->Vypis();
+                    ?></svg>
                     <span class="priklad">lorem ipsum</span><br>
-                    <svg width="120" height="120" class="segment" id=""></svg>
+                    <svg width="120" height="120" class="segment" id=""><?php
+                    $segment15 = new Segment(15, 'test');
+                    $segment15->Vypis();
+                    ?></svg>
                     <span class="priklad">lorem ipsum</span><br>
                 </div>
             </div>
         </div>
-        <?php
-        // put your code here
-        ?>
+       
     </body>
 </html>
