6da0befb60a8597ef813b90a1350b7b4202bbac4
vaclav <votrv1ap@pedf.cuni.cz>
2019-01-06 16:09:06 +0100

    admin page

---
+++ adminTajny.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+>
+<html>
+    <html>
+    <head>
+        <meta charset="UTF-8">
+        <title>matematický trenažer</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="source/MPstyle.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="jumbotron text-center">
+            <h1>matematický trenažér</h1>
+            <p>admine, připrav hru...</p> 
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12, plocha"></div>
+    </body>
+</html>
